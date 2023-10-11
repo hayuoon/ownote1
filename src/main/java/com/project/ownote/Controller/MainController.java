6f3b3b26d8a784449892caa7a6eed4fada8e36b9
hej9700 <haeun970@naver.com>
2023-10-11 17:27:36 +0900

    출근,퇴근

--- src/main/java/com/project/ownote/Controller/MainController.java
+++ src/main/java/com/project/ownote/Controller/MainController.java
@@ -1,18 +1,23 @@
 package com.project.ownote.Controller;
 
+import com.project.ownote.emp.login.dto.AuthInfo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String main(){
+    public String main(HttpSession session, Model model){
+        AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+        model.addAttribute("authInfo", authInfo);
         return "ownote";
     }
 
     @GetMapping("/ownote")
-    public String ownote() {
-        return "ownote";
-    }
+    public String ownote() {return "ownote";}
+
 }
