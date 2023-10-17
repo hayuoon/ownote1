788f8a7cf37bbe6839cf6545c2b4a098fe28adef
hej9700 <haeun970@naver.com>
2023-10-17 14:11:40 +0900

    css 적용

--- src/main/resources/static/js/music.js
+++ src/main/resources/static/js/music.js
@@ -1,37 +1,9 @@
 $(document).ready(function () {
-    $('.listback-link').click(function (e) {
-        e.preventDefault(); // 기본 링크 동작을 막습니다.
-
-        var clubboardId = $(this).data('clubboard-id');
-        console.log(clubboardId);
-
-        // 확인 대화 상자 표시
-        var confirmResult = confirm('작성 중인 게시글은 저장되지 않습니다.');
-
-        if (confirmResult) {
-            // 사용자가 확인을 누른 경우에만 Ajax 요청을 보냅니다.
-            $.ajax({
-                type: 'GET', // 또는 'GET' 요청 방식
-                url: '/club/list', // 컨트롤러의 URL을 적어주세요.
-                success: function (data) {
-                    console.log(data);
-                    // 성공적으로 처리한 후의 동작
-                    window.location.href = '/club/list';
-                },
-                error: function (x) {
-                    console.log(x);
-                    // 오류 발생 시 처리
-                    alert('게시글을 불러오는 중 오류가 발생했습니다.');
-                }
-            });
-        }
-    });
-
     $('.delete-link').click(function (e) {
         e.preventDefault(); // 기본 링크 동작을 막습니다.
 
-        var clubboardId = $(this).data('clubboard-id');
-        console.log(clubboardId);
+        let musiccontest_id = $(this).data('musiccontest_id');
+        console.log(musiccontest_id);
 
         // 확인 대화 상자 표시
         var confirmResult = confirm('게시글을 삭제하시겠습니까?');
@@ -40,12 +12,12 @@ $(document).ready(function () {
             // 사용자가 확인을 누른 경우에만 Ajax 요청을 보냅니다.
             $.ajax({
                 type: 'GET', // 또는 'GET' 요청 방식
-                url: '/club/delete', // 컨트롤러의 URL을 적어주세요.
-                data: { clubboardId: clubboardId }, // 컨트롤러로 전달할 데이터
+                url: '/musicContest/delete', // 컨트롤러의 URL을 적어주세요.
+                data: { musiccontest_id: musiccontest_id }, // 컨트롤러로 전달할 데이터
                 success: function (data) {
                     console.log(data);
                     // 성공적으로 처리한 후의 동작
-                    window.location.href = '/club/list';
+                    window.location.href = '/musicContest/list';
                     alert('게시글이 삭제되었습니다.');
                 },
                 error: function (x) {
